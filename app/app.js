6be99ea378fe9d1f37a4791c136b235aa7276076
remon <remonmizanur@gmail.com>
2023-03-16 00:40:18 +0600

    updt

--- app/app.js
+++ app/app.js
@@ -17,7 +17,7 @@ myApp.config(['$routeProvider', function ($routeProvider) {
 
 myApp.controller('myController', ["$scope", "$http", function ($scope, $http) {
 
-    $scope.msg = "Animals:: ";
+    $scope.title = "NEW ARRIVAL";
 
     $scope.datas = ['cow', 'sheep', 'goat'];
 
@@ -42,41 +42,17 @@ myApp.controller('myController', ["$scope", "$http", function ($scope, $http) {
 
     }
 
-    /*$scope.products = [{
-            name: "Shirt",
-            types: ['XL', 'L', 'XXL'],
-            rating: 4.5,
-            color: ['green', 'red', 'yellow'],
-            image: "https://images-ssl.gotinder.com/6337215eedb6ed0100496fb5/original_c17967e2-52d6-4b3b-9b74-23b2463cfef1.jpeg"
-        },
-        {
-            name: "Polo",
-            types: ['XL', 'L', 'XXL'],
-            rating: 3.7,
-            color: ['green', 'red', 'yellow'],
-            image: "https://images-ssl.gotinder.com/6337215eedb6ed0100496fb5/320x400_08eea09d-b3aa-4ec3-9c82-960a374c74db.jpg"
-        },
-        {
-            name: "T Shirt",
-            types: ['XL', 'L', 'XXL'],
-            rating: 4.2,
-            color: ['green', 'red'],
-            image: "https://images-ssl.gotinder.com/6337215eedb6ed0100496fb5/original_08eea09d-b3aa-4ec3-9c82-960a374c74db.jpeg"
-        },
-    ]
-    console.log(angular.toJson($scope.products))
-    $http.get('../data/products.json').success(function (data) {
-
-        $scope.products = data
-
-        console.log("hi")
-    });*/
-
     $http({
         method: 'GET',
-        // url: '../data/products.json'
-        url: 'https://dummyjson.com/products?limit=10'
+        url: "https://dummyjson.com/products?limit=20",
+        //url:"https://dummyapi.io/data/v1/post?limit=10",
+        // headers: {
+        //     "app-id": "63cd111ff805cea1c4e8e6a3",
+
+        // }
     }).then(function (response) {
+
+        //console.log(angular.toJson(response))
         console.log(response, 'res');
         $scope.products = response.data['products'];
     }, function (error) {
